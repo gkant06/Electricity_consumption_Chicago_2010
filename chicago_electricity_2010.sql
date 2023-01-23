326a385f5ca34bc58c0e1da152cc5cb6b307ad86
gkant06 <105669929+gkant06@users.noreply.github.com>
2023-01-22 20:07:29 -0500

    Add files via upload

--- chicago_electricity_2010.sql
+++ chicago_electricity_2010.sql
@@ -1,4 +1,4 @@
--- Viewing entire
+-- Viewing the data
 select * 
 from project1;
 
@@ -6,11 +6,11 @@ from project1;
 select sum(total) 
 from project1;
 
--- Total consumption by neighbourhood
+-- Total consumption and % of total consumption by neighbourhood
 select 
 sub.community_area_name,
 sub.community_total,
-cast(cast(sub.community_total as dec(2,0))/cast(sum(sub.community_total) as dec(2,0)) as dec(2,0)) as perc
+cast(sub.community_total as numeric(15,3))/(select sum(total) from project1) as perc
 from (
 select community_area_name, 
        sum(total) as community_total 
@@ -19,15 +19,37 @@ group by community_area_name) sub
 group by sub.community_area_name, sub.community_total
 order by perc desc;
 
--- Total consumption by building type
-select building_type, sum(total) as building_total
+-- Total consumption and % of total consumption by building type
+select 
+sub.building_type,
+sub.building_total,
+cast(sub.building_total as numeric(20,3))/(select sum(total) from project1) as perc
+from (
+select building_type, 
+       sum(total) as building_total 
 from project1
-group by building_type
-order by building_total desc;
+group by building_type) sub
+group by sub.building_type, sub.building_total
+order by perc desc;
 
--- Total consumption by building sub type
-select building_subtype, sum(total) as building_sub_total
+-- Total consumption and % of total consumption by building sub-type
+select 
+sub.building_subtype,
+sub.building_subtotal,
+cast(sub.building_subtotal as numeric(20,3))/(select sum(total) from project1) as perc
+from (
+select building_subtype, 
+       sum(total) as building_subtotal 
 from project1
-group by building_subtype
-order by building_sub_total desc;
+group by building_subtype) sub
+group by sub.building_subtype, sub.building_subtotal
+order by perc desc;
+
+
+-- Total monthly consumption (in 10^6 KWh) trend
+select sum(jan)/1000000 as jan, sum(feb)/1000000 as feb, sum(mar)/1000000 as mar,
+       sum(apr)/1000000 as apr, sum(may)/1000000 as may, sum(jun)/1000000 as jun,
+	   sum(jul)/1000000 as jul, sum(aug)/1000000 as aug, sum(sep)/1000000 as sep,
+	   sum(oct)/1000000 as oct, sum(nov)/1000000 as nov, sum(dec)/1000000 as dec
+from project1
 
