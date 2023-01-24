8d48f9e7d95c90f1d9a30c35f3b5314e67a25c28
gkant06 <105669929+gkant06@users.noreply.github.com>
2023-01-24 17:22:44 -0500

    Add files via upload

--- chicago_electricity_2010.sql
+++ chicago_electricity_2010.sql
@@ -1,10 +1,24 @@
 -- Viewing the data
+
+-- Contains electricity consumption information
 select * 
 from project1;
 
--- Total electricity use in 2010
-select sum(total) 
+-- Contains gas consumption information
+select * 
+from gasconsumption;
+
+-- Contains housing information and other attributes such as population
+select * 
+from buildinginfo;
+
+-- Total electricity use (10^6 kWh) in 2010
+select sum(total)/1000000 as total_elecricity
 from project1;
+
+-- Total therms of gas used in 2010
+select sum(total) as total_gas
+from gasconsumption;
 
 -- Total consumption and % of total consumption by neighbourhood
 select 
@@ -18,6 +32,14 @@ from project1
 group by community_area_name) sub
 group by sub.community_area_name, sub.community_total
 order by perc desc;
+
+-- Average total population by community area
+select avg(sub.total_population) as overall_avg_pop
+from(
+select community_area_name,
+       sum(population) as total_population
+from buildinginfo
+group by community_area_name)sub;
 
 -- Total consumption and % of total consumption by building type
 select 
@@ -51,5 +73,12 @@ select sum(jan)/1000000 as jan, sum(feb)/1000000 as feb, sum(mar)/1000000 as mar
        sum(apr)/1000000 as apr, sum(may)/1000000 as may, sum(jun)/1000000 as jun,
 	   sum(jul)/1000000 as jul, sum(aug)/1000000 as aug, sum(sep)/1000000 as sep,
 	   sum(oct)/1000000 as oct, sum(nov)/1000000 as nov, sum(dec)/1000000 as dec
+from project1
+
+-- Total quarterly consumption(in 10^6 KWh) trend
+select sum(jan+feb+mar)/1000000 as Q1,
+       sum(apr+may+jun)/1000000 as Q2,
+	   sum(jul+aug+sep)/1000000 as Q3,
+	   sum(oct+nov+dec)/1000000 as Q4
 from project1
 
