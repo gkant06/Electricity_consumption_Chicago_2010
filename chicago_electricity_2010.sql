9a59c3c43efbe743bf777954e01ce8d1b8eb1ab9
gkant06 <105669929+gkant06@users.noreply.github.com>
2023-02-02 18:41:22 -0500

    Add files via upload

--- chicago_electricity_2010.sql
+++ chicago_electricity_2010.sql
@@ -1,84 +1,103 @@
 -- Viewing the data
 
 -- Contains electricity consumption information
-select * 
-from project1;
+SELECT * 
+FROM project1;
 
 -- Contains gas consumption information
-select * 
-from gasconsumption;
+SELECT * 
+FROM gasconsumption;
 
 -- Contains housing information and other attributes such as population
-select * 
-from buildinginfo;
+SELECT * 
+FROM buildinginfo;
 
 -- Total electricity use (10^6 kWh) in 2010
-select sum(total)/1000000 as total_elecricity
-from project1;
+SELECT SUM(total)/1000000 AS total_elecricity
+FROM project1;
 
 -- Total therms of gas used in 2010
-select sum(total) as total_gas
-from gasconsumption;
+SELECT SUM(total) AS total_gas
+FROM gasconsumption;
 
 -- Total consumption and % of total consumption by neighbourhood
-select 
+SELECT 
 sub.community_area_name,
 sub.community_total,
-cast(sub.community_total as numeric(15,3))/(select sum(total) from project1) as perc
-from (
-select community_area_name, 
-       sum(total) as community_total 
-from project1
-group by community_area_name) sub
-group by sub.community_area_name, sub.community_total
-order by perc desc;
+CAST(sub.community_total AS numeric(15,3))/(SELECT SUM(total) FROM project1) AS perc
+FROM (
+SELECT community_area_name, 
+       SUM(total) AS community_total 
+FROM project1
+GROUP BY community_area_name) sub
+GROUP BY sub.community_area_name, sub.community_total
+ORDER BY perc DESC;
 
--- Average total population by community area
-select avg(sub.total_population) as overall_avg_pop
-from(
-select community_area_name,
-       sum(population) as total_population
-from buildinginfo
-group by community_area_name)sub;
+-- Average total population per community area
+SELECT AVG(sub.total_population) AS overall_avg_pop
+FROM(
+SELECT community_area_name,
+       SUM(population) AS total_population
+FROM buildinginfo
+GROUP BY community_area_name)sub;
+
+-- Create a flag for community areas with above and below average population. 
+-- Get a count of communities with population above and below average
+SELECT sub2.population_flag,
+       COUNT(sub2.community_area_name) AS count_community_area_name
+FROM(
+SELECT sub.community_area_name, sub.community_total, 
+CASE
+ WHEN sub.community_total >= AVG(sub.community_total) THEN 'Above average'
+ WHEN sub.community_total < AVG(sub.community_total) THEN 'Below average'
+END population_flag
+FROM(
+SELECT community_area_name,
+       SUM(population) AS community_total
+FROM buildinginfo
+GROUP BY community_area_name)sub
+GROUP BY sub.community_area_name, sub.community_total) sub2
+GROUP BY sub2.population_flag;
+
 
 -- Total consumption and % of total consumption by building type
-select 
+SELECT
 sub.building_type,
 sub.building_total,
-cast(sub.building_total as numeric(20,3))/(select sum(total) from project1) as perc
-from (
-select building_type, 
-       sum(total) as building_total 
-from project1
-group by building_type) sub
-group by sub.building_type, sub.building_total
-order by perc desc;
+CAST(sub.building_total AS numeric(20,3))/(SELECT SUM(total) FROM project1) AS perc
+FROM (
+FROM building_type, 
+       SUM(total) AS building_total 
+FROM project1
+GROUP BY building_type) sub
+GROUP BY sub.building_type, sub.building_total
+ORDER BY perc DESC;
 
 -- Total consumption and % of total consumption by building sub-type
-select 
+SELECT 
 sub.building_subtype,
 sub.building_subtotal,
-cast(sub.building_subtotal as numeric(20,3))/(select sum(total) from project1) as perc
-from (
-select building_subtype, 
-       sum(total) as building_subtotal 
-from project1
-group by building_subtype) sub
-group by sub.building_subtype, sub.building_subtotal
-order by perc desc;
+CAST(sub.building_subtotal AS numeric(20,3))/(SELECT SUM(total) FROM project1) AS perc
+FROM (
+SELECT building_subtype, 
+       SUM(total) AS building_subtotal 
+FROM project1
+GROUP BY building_subtype) sub
+GROUP BY sub.building_subtype, sub.building_subtotal
+ORDER BY perc DESC;
 
 
 -- Total monthly consumption (in 10^6 KWh) trend
-select sum(jan)/1000000 as jan, sum(feb)/1000000 as feb, sum(mar)/1000000 as mar,
-       sum(apr)/1000000 as apr, sum(may)/1000000 as may, sum(jun)/1000000 as jun,
-	   sum(jul)/1000000 as jul, sum(aug)/1000000 as aug, sum(sep)/1000000 as sep,
-	   sum(oct)/1000000 as oct, sum(nov)/1000000 as nov, sum(dec)/1000000 as dec
-from project1
+SELECT SUM(jan)/1000000 AS jan, SUM(feb)/1000000 AS feb, SUM(mar)/1000000 AS mar,
+       SUM(apr)/1000000 AS apr, SUM(may)/1000000 AS may, SUM(jun)/1000000 AS jun,
+	   SUM(jul)/1000000 AS jul, SUM(aug)/1000000 AS aug, SUM(sep)/1000000 AS sep,
+	   SUM(oct)/1000000 AS oct, SUM(nov)/1000000 AS nov, SUM(dec)/1000000 AS dec
+FROM project1
 
 -- Total quarterly consumption(in 10^6 KWh) trend
-select sum(jan+feb+mar)/1000000 as Q1,
-       sum(apr+may+jun)/1000000 as Q2,
-	   sum(jul+aug+sep)/1000000 as Q3,
-	   sum(oct+nov+dec)/1000000 as Q4
-from project1
+SELECT SUM(jan+feb+mar)/1000000 AS Q1,
+       SUM(apr+may+jun)/1000000 AS Q2,
+	   SUM(jul+aug+sep)/1000000 AS Q3,
+	   SUM(oct+nov+dec)/1000000 AS Q4
+FROM project1
 
